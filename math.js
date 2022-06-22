9477c75ec9ec2e7269aae0083ca2fcc1b37b6538
“Pauloviictorss” <paulovictorlvm@gmail.com>
2022-06-22 20:31:31 -0300

    Inserindo operações aritméticas

--- math.js
+++ math.js
@@ -1,19 +1,15 @@
 function soma(a, b) {
-    console.log(a + b)
     return a + b
 }
 function subtracao(a, b) {
-    console.log(a - b)
     return a - b
 }
 
 function divisao(a, b) {
-    console.log(a / b)
     return a / b
 }
 
 function multiplicacao(a, b) {
-    console.log(a * b)
     return a * b
 }
 
